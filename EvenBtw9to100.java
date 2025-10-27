2604e7a2f1452f2426e3fa00943f9248f3ca83d0
jgdips4 <jagdeepsingh@Jagdeeps-MacBook-Air.local>
2025-10-28 04:44:59 +0530

    3rd day of DSA

---
+++ EvenBtw9to100.java
@@ -0,0 +1,10 @@
+public class EvenBtw9to100{
+    public static void main(String[] args){
+        int n = 9;
+        while(n<100){
+            if(n%2==0){
+                System.out.println(n);
+            }n++;
+            }
+        }
+    }
