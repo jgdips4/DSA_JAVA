3c5db87ac374565521e96301ebcedd42d08e9ed0
jgdips4 <jagdeepsingh@Jagdeeps-MacBook-Air.local>
2025-11-21 05:01:56 +0530

    19th Day of DSA

--- BubbleSort.java
+++ BubbleSort.java
@@ -24,7 +24,7 @@ public class BubbleSort {
         }System.err.println("");
     }
 
-
+    // selection sort
     public static void selectionSort(int arr[]){
         for(int i=0; i<arr.length-1; i++){
             int miniPos = i;
@@ -39,6 +39,23 @@ public class BubbleSort {
         arr[i]  = temp;
         }
         
+    }
+
+    // insertionSort
+
+    public static void insertionSort(int arr[]) {
+        for(int i=1; i<arr.length; i++){
+        int curr = arr[i];
+        int prev = i-1;
+
+        while(prev<0 && arr[prev]> curr){
+            arr[prev+1]= arr[prev];
+            prev--;
+        }
+        // insertion
+        arr[prev+1]=curr;
+
+        }
     }
     public static void main(String[] args) {
         int arr[] = { 3, 4, 1, 7, 8};
---
+++ SelectionSort.java
@@ -0,0 +1,29 @@
+public class SelectionSort {
+    static void selectionSort(int arr[]){
+         for(int i=0; i<arr.length-1; i++){
+            int miniPos = i;
+            for(int j=i+1; j<arr.length; j++){
+                
+                if(arr[miniPos]> arr[j])
+                miniPos = j;
+            }
+            //swap
+            int temp = arr[miniPos];
+            arr[miniPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i]);
+        }
+        System.out.println("");
+    }
+    public static void main(String[] args) {
+        int arr[] = {3,5,2,1,7,6};
+        selectionSort(arr);
+        printArray(arr);
+
+    }
+}
